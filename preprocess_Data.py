93a1878598189fabbcf4b09c0c4d86ea01d3a8af
kushal12345 <hipopgangster@gmail.com>
2019-02-16 12:30:17 +0545

    Backpropagation

---
+++ preprocess_Data.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+admit = pd.read_csv('Entrance_Selection.csv')
+
+# Make dummy variables for rank
+
+data = pd.concat([admit, pd.get_dummies(admit['rank'], prefix='rank')], axis=1)
+data = data.drop('rank', axis=1)
+
+
+# Standarize features
+
+for field in ['gre', 'gpa']:
+    mean, std = data[field].mean(), data[field].std()
+    data.loc[:, field] = (data[field] - mean) / std  # Computing the Z score
+
+
+# Split off random 10% of the data for testing
+
+np.random.seed(42)
+sample = np.random.choice(data.index, size=int(len(data) * 0.9), replace=False)
+data, test_data = data.ix[sample], data.drop(sample)
+
+
+# Split into features and targets
+
+features, targets = data.drop('admit', axis=1), data['admit']
+features_test, targets_test = test_data.drop('admit', axis=1), test_data['admit']
+print(features)
